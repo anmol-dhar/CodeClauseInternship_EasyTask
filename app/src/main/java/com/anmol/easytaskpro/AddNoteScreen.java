26b2bc9631b7705c27cb485972f506adeb010c7a
Anmol Dhar <anmoldhar4@gmail.com>
2023-11-20 00:33:33 +0530

    Homepage ui and add new task ui created.

---
+++ app/src/main/java/com/anmol/easytaskpro/AddNoteScreen.java
@@ -0,0 +1,14 @@
+package com.anmol.easytaskpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNoteScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note_screen);
+    }
+}
--- app/src/main/java/com/anmol/easytaskpro/MainActivity.java
+++ app/src/main/java/com/anmol/easytaskpro/MainActivity.java
@@ -2,13 +2,28 @@ package com.anmol.easytaskpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView addTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addTask = findViewById(R.id.addTaskButton);
+
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddNoteScreen.class));
+            }
+        });
+
     }
 }
